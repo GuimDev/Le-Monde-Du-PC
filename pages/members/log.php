96fdd762c86dde19f5ab6a2e3e6646dc894d0f4f
Niels <contact@lemondedupc.fr>
2018-06-13 20:29:39 +0200

    modif

--- pages/members/log.php
+++ pages/members/log.php
@@ -2,12 +2,5 @@
 // file to log in and log out
 // can log with the email or the login
 require __DIR__.'/src/php/functions.php'; 
-session_start();
-maintenance_mode();
-if(isset($_SESSION['id'])){
-
-} else {
-	
-}
 
 ?>
