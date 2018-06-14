a05ee38baeb5abee5c1330852dc16b45b6cb670e
Niels <contact@lemondedupc.fr>
2018-06-14 20:46:31 +0200

    modif code de connexion et déconnexion

--- pages/members/log.php
+++ pages/members/log.php
@@ -7,19 +7,27 @@ maintenance_mode();
 
 if(isset($_GET['action']) AND empty($_GET['action']) === false){
 	$action = strval($_GET['action']);
-	if($action === 'connexion'){ // connexion
-		if(isset($_SESSION['user_connected']) AND empty($_SESSION['user_connected'])){ // if the user if already connected
-			
-		} else { // if the user if not already connected
-			// connexion
-			
+	if(isset($_SESSION['user_connected']) AND empty($_SESSION['user_connected'])){ // if the user is already connected
+		if($action === 'deconnexion'){ // deconnexion
+			$_SESSION = array();
+			if(ini_get('session.use_cookies')){ // destroy all the session
+				$params = session_get_cookie_params();
+    			setcookie(session_name(), '', time() - 42000,
+        			$params['path'], $params['domain'],
+        			$params['secure'], $params['httponly']
+    			);
+			}
+		} else { // redirection
+
 		}
-	} else if($action === 'deconnexion'){ // deconnexion
-		
-	} else { // error, not a valid parameter 
-		
+	} else { 
+		if($action === 'connexion'){ // connexion
+
+		} else { // error, not a good parameter
+
+		}
 	}
-} else { // error, no parameter or empty
+} else { // error, no parameter or empty parameter
 
 }
 ?>
